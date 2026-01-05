4bf47bf3461de1d8389a99780f4a5c2506e019d8
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-05 10:18:54 +0700

    char data type

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char a = 'A';
+        char r = 'R';
+        char y = 'Y';
+
+        System.out.println(a);
+        System.out.println(r);
+        System.out.println(y);
+    }
+}
