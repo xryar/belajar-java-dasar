71491d032001c649f13b32aac0111ad1e802cc16
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-05 09:54:39 +0700

    number conversion

---
+++ src/KonversiNumber.java
@@ -0,0 +1,13 @@
+public class KonversiNumber {
+
+    public static void main(String[] args) {
+        // automatic conversion
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+
+        // manual conversion
+        byte iniByte2 = (byte) iniInt;
+    }
+
+}
