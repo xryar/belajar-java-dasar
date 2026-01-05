b12fc1301eae6c3fcbfca0dd19b4a2ce1233abae
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-05 09:20:49 +0700

    various number data types in java

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,24 @@
+public class TipeDataNumber {
+
+    public static void main(String[] args) {
+        // int
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 100000;
+        long iniLong = 10000000;
+        long iniLong2 = 10000000L;
+
+        // floating
+        float iniFloat = 10.10f;
+        double iniDouble = 12.1200;
+
+        // literals
+        int intDecimal = 34;
+        int hexDecimal = 0xFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        // underscore
+        int amount = 1_000_000_000;
+    }
+
+}
