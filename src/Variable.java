d4913bf77d6c488c87ec3fbdef1669e24d1869fe
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-07 09:15:17 +0700

    variable in java

---
+++ src/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Arya Rizki Andaru";
+
+        int age = 30;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Ucogs Nugraha";
+        System.out.println(name);
+
+        // kalau malas inisiasi variabel yang ada tipe datanya bisa langsung pake var.
+        // tapi, harus langsung di isi valuenya
+        var firstName = "Arya";
+        var middleName = "Rizki";
+        var lastName = "Andaru";
+
+        // const in java
+        final String application = "Belajar Java PZN";
+    }
+}
