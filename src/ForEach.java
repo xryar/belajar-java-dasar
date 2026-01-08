0881c37fc76256ebe3651ca0f7de899c2f5ab691
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-08 11:06:35 +0700

    for each

---
+++ src/ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] names = {
+                "Arya", "Reza", "Ucogs",
+                "Programmer", "Zaman", "Now"
+        };
+
+        for (var value: names) {
+            System.out.println(value);
+        }
+    }
+}
