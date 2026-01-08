3ab7bc075ea81c0f46c25b59874bcf871f97ae77
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-08 10:49:11 +0700

    do while loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        do {
+            System.out.println("Counter " + counter);
+            counter++;
+        } while (counter <= 10);
+    }
+}
