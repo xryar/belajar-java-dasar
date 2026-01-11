c4040576407c183e3efc127c068092669052966e
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-11 17:10:23 +0700

    method return parameter

---
+++ src/MethodReturnValue.java
@@ -0,0 +1,14 @@
+public class MethodReturnValue {
+    public static void main(String[] args) {
+        var a = 100;
+        var b = 100;
+        var c = sum(a, b);
+
+        System.out.println(c);
+    }
+
+    static int sum(int value1, int value2) {
+        var total = value1 + value2;
+        return total;
+    }
+}
