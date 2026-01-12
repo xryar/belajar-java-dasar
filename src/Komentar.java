b11b2b160cf92b83f0fae9dd95cd9fbb212d36c6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2026-01-12 11:17:56 +0700

    java comment

---
+++ src/Komentar.java
@@ -0,0 +1,17 @@
+public class Komentar {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Method untuk menjumlahkan value 1 dan 2
+     * @param value1 angka pertama
+     * @param value2 angka kedua
+     * @return hasil penjumlahan value1 dan 2
+     */
+
+    static int sum(int value1, int value2) {
+        // jumlahkan value 1 dan value 2
+        return value1 + value2;
+    }
+}
